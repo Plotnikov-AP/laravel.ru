2e7e87d923a0f12a2a82ae70e0a2e35956e51fca
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-06-03 19:44:00 +0300

    add file routes/web.php

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Counter;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\BasketController;
+
+
+Route::get('/', [MainController::class, 'main']);
+Route::get('/main', [MainController::class, 'main'])->name('main');
+Route::get('/author', [MainController::class, 'author'])->name('author');
+Route::get('/testworks', [MainController::class, 'testWorks'])->name('testworks');
+Route::get('/shop', [MainController::class, 'shop'])->name('shop');
+Route::get('/shop/product/{id}', [MainController::class, 'productId']);
+Route::get('/basket/show', [BasketController::class, 'show'])->name('show');
+
+
+
