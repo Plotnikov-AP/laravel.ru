3122d36cdc4170b264932b7189e03be1e280729f
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-06-06 08:56:38 +0300

    edit goto basket blade

--- resources/views/components/goto-basket.blade.php
+++ resources/views/components/goto-basket.blade.php
@@ -1,4 +1,4 @@
 <div class="goto-basket">
-    <span id="basket-count"></span>
+    <a href="{{ route('show') }}"><span id="basket-count"></span></a>
     <a href="{{ route('show') }}"><img src="/images/free-icon-shopping-cart-4033026(1).png" alt="Перейти в корзину"/></a>
 </div>
