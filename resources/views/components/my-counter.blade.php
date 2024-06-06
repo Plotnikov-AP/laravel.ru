404155bb595a8cce77e9189919be8cfac0b845c5
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-06-06 17:56:25 +0300

    edit my-counter.blade.php

--- resources/views/components/my-counter.blade.php
+++ resources/views/components/my-counter.blade.php
@@ -1,7 +1,7 @@
 <div class="count">
     <span id="span-count-all"></span>
     <span id="span-count-desktop"></span>
-    <span id="span-count-dphone"></span>
+    <span id="span-count-phone"></span>
     <span id="span-count-tablet"></span>
     <span id="span-count-robot"></span>
 </div>
