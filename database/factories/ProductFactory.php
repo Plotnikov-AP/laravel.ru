c2240f84f34e8f4d10024860f6107002ccee8f1b
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-06-18 15:11:58 +0300

    edit ProductFactiry,php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,12 +16,13 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
-        $photo=['/images/product/product.jpg', '/images/product/product1.jpg', '/images/product/product2.png', '/images/product/product3.jpg'];
+        $photo=['/laravel.ru/images/products/phone.png', '/laravel.ru/images/products/tablet.jpg', '/laravel.ru/images/products/televizor.jpg', '/laravel.ru/images/products/microwave.jpg', '/laravel.ru/images/products/fridge.png', '/laravel.ru/images/products/washing machine.png'];
         $product=['смартфон', 'планшет', 'телевизор', 'микроволновка', 'холодильник', 'стиральная машина'];
+        $number=mt_rand(0, 5);
         return [
-            'name'=>$product[mt_rand(0, 5)],
-            'photo'=>$photo[mt_rand(0, 3)],
-            'price'=>mt_rand(100, 100000),
+            'name'=>$product[$number],
+            'photo'=>$photo[$number],
+            'price'=>mt_rand(1000, 50000),
             'description'=>$this->faker->text(mt_rand(100, 200)),
             'on_sklad'=>mt_rand(0, 50)
         ];
