95d46c3f88ae4715a52a3ba8cdabefdaefa68be5
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-06-29 16:18:11 +0300

    add auth

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="{{ asset('/css/styles.css') }}">
+    <!-- Scripts -->
     <script src="{{ asset('/js/jquery.js') }}"></script>
     <script src="{{ asset('/js/sweetalert2.all.min.js') }}"></script>
     <script src="{{ asset('/js/myjs.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title }}</title>
 </head>
 <body onload="onloadd()";>
     <div id="header">
         Это тестовый сайт Плотникова Александра на LARAVEL
+        @include('layouts.my-login')
         <x-my-menu />
     </div>
     <div id="left-main">
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Models\Counter;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\BasketController;
-
-
-Route::get('/', [MainController::class, 'main']);
-Route::get('/author', [MainController::class, 'author'])->name('author');
-Route::get('/testworks', [MainController::class, 'testWorks'])->name('testworks');
-Route::get('/shop', [MainController::class, 'shop'])->name('shop');
-Route::get('/shop/product/{id}', [MainController::class, 'productId']);
+use Illuminate\Http\Request;                                                              
+use App\Models\Counter;                                                                   
+use App\Http\Controllers\MainController;                                                  
+use App\Http\Controllers\BasketController;                                                
+                                                                                          
+                                                                                          
+Route::get('/', [MainController::class, 'main']);                                         
+Route::get('/author', [MainController::class, 'author'])->name('author');                 
+Route::get('/testworks', [MainController::class, 'testWorks'])->name('testworks');        
+Route::get('/shop', [MainController::class, 'shop'])
+->middleware('auth')
+->name('shop');                       
+Route::get('/shop/product/{id}', [MainController::class, 'productId']);                   
 Route::get('/basket/show', [BasketController::class, 'basket_show'])->name('basket_show');
 
 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
