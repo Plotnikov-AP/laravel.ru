4bac4cb7b30b83c59558919d415299dd14184bc9
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-07-08 20:43:19 +0300

    dell br in droduct.blade.php

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,7 +1,7 @@
 <div class="product">
-    <img src="{{ $photo }}" /><br />
-    <label>Название товара: <b>{{ $name }}</b></label><br />
-    <label>Цена товара: <b>{{ $price }} </b>рублей</label><br />
+    <img src="{{ $photo }}" />
+    <label><b>{{ $name }}</b></label><br />
+    <label>Цена: <b>{{ $price }} </b>рублей</label><br />
     <button onclick="basket_add({{ $id }})">Добавить в корзину</button><br />
     <button class="show_div_detail" onclick="show_div_detail({{ $id }})">Подробнее</button>
 </div>
