fa7271d13d9418e4b2a7a00e4ca72b10cf86c77a
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-07-22 12:23:41 +0300

    main add paytnashki

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="{{ asset('/css/styles.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('/css/pyatnashki.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('/js/jquery.js') }}"></script>
     <script src="{{ asset('/js/sweetalert2.all.min.js') }}"></script>
@@ -16,6 +17,7 @@
         <div class="display_flex">
             Это тестовый сайт Плотникова Александра на LARAVEL 11.14.0
             @include('layouts.my-login')
+            <!-- @include('components.my_buttom_ru_en') -->
         </div>
         <x-my-menu />
     </div>
