8f9d858aed72089aa1b81f84ee5b15a471fd706f
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-07-23 06:13:34 +0300

    pyatnashki title and description

--- resources/views/my_works/pyatnashki/pyatnashki.blade.php
+++ resources/views/my_works/pyatnashki/pyatnashki.blade.php
@@ -1,3 +1,9 @@
-    <div class="contayner_for_pyatnashki">
-        <script src="js/pyatnashki.js"></script>
-    </div>
+<div class="pyatnashki_title">
+    Старые, добрые ПЯТНАШКИ... (на JavaScript)
+</div>
+<div class="contayner_for_pyatnashki">
+    <script src="js/pyatnashki.js"></script>
+</div>
+<div class="pyatnashki_description">
+    Правила, надеюсь, всем известны... Меняем блоки по горизонтали и вертикали, пока числа не выстроятся по порядку.
+</div>
