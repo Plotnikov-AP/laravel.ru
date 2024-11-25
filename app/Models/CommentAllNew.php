2c282e8e2fb3487ba12f14d24422f79abf694acb
Plotnikov Alexandr <plotnikov-ap@yandex.ru>
2024-11-25 20:21:24 +0300

    add model commentallnew

---
+++ app/Models/CommentAllNew.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentAllNew extends Model
+{
+    use HasFactory;
+}
